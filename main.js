d4f9f9ed5c5a33b2f1b97bc819aa9b2820c2a964
fellayoucef <youceff.fella@gmail.com>
2018-12-23 21:47:14 +0100

    progbar

---
+++ main.js
@@ -0,0 +1,51 @@
+
+window.onload = function onLoad() {
+    var bar = new ProgressBar.Line('#progress', {
+        strokeWidth: 5,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#848484',
+        trailColor: '#eee',
+        trailWidth: 1,
+        
+      });
+      
+      bar.animate(0.5);
+
+      var bar = new ProgressBar.Line('#progress2', {
+        strokeWidth: 5,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#848484',
+        trailColor: '#eee',
+        trailWidth: 1,
+        
+      });
+      
+      bar.animate(0.8);
+
+      var bar = new ProgressBar.Line('#progress3', {
+        strokeWidth: 5,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#848484',
+        trailColor: '#eee',
+        trailWidth: 1,
+        
+      });
+      
+      bar.animate(0.9);
+
+      var bar = new ProgressBar.Line('#progress4', {
+        strokeWidth: 5,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#848484',
+        trailColor: '#eee',
+        trailWidth: 1,
+        
+      });
+      
+      bar.animate(0.9);
+      
+};
